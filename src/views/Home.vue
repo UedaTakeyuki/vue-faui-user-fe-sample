473144c07bc76e3b1b9b402f4388fefcfce5ac85
Takeyuki UEDA <ueda@latelierdueda.com>
2021-03-27 19:51:00 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <AccountFront />
+    <Payments />
+  </div>
+</template>
+
+<script>
+import vfuf from "vue-faui-user-fe"
+var AccountFront = vfuf.AccountFront
+var Payments = vfuf.Payments
+export default {
+  components: {
+    AccountFront,
+    Payments
+  },
+  mounted(){
+    console.log(AccountFront)
+    console.log(vfuf)
+  }
+};
+</script>
