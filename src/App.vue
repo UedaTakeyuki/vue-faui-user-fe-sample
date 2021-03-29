11420482b00a03c39acaf8bc38f82e7a13c9c93e
Takeyuki UEDA <ueda@latelierdueda.com>
2021-03-29 16:22:33 +0900

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-
+    <Navbar />
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -8,9 +8,10 @@
 </template>
 
 <script>
-
+import Navbar from '@/components/Navbar'
 export default {
   name: 'App',
+  components: { Navbar },
   data: () => ({
     //
   }),
