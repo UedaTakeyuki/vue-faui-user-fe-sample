4cf2af378120f7cb8a71018f9e65ba46bacc480c
Takeyuki UEDA <ueda@latelierdueda.com>
2021-03-29 19:36:14 +0900

    use qrcode-of-this-site

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -33,6 +33,7 @@
 
     <v-navigation-drawer v-model="drawer" app class="primary">
       <v-list>
+        <QRcode/>
         <!-- v-list-tile is changed to v-list-item -->
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action>
@@ -49,7 +50,9 @@
 
 
 <script>
+import {QRcode} from 'qrcode-of-this-site'
 export default {
+  components: {QRcode},
   data: () => ({
     drawer: false,
     links: [
