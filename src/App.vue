9863ed2eccfd972896cf0cf34ecc1b19389ce302
Takeyuki UEDA <ueda@latelierdueda.com>
2021-05-26 09:40:00 +0900

    use vuetify-nav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app>
-    <Navbar />
+    <Navbar titleStr="毛呂山" :links="links">
+    </Navbar>
+<!--    <Navbar /> -->
     <Login/>
     <v-content>
       <router-view></router-view>
@@ -9,18 +11,24 @@
 </template>
 
 <script>
-import {AccountRoutes} from 'vue-faui-user-fe/'
+import {AccountRoutes, PurchaseRoutes} from 'vue-faui-user-fe/'
+import {Navbar} from 'vuetify-nav'
 
 //import "firebaseui/dist/firebaseui.css";
 //import firebase from "firebase/app";
 
 //import { Login } from 'vue-faui-user-fe/'
-import Navbar from '@/components/Navbar'
+//import Navbar from '@/components/Navbar'
 import Login from '@/components/Login'
 export default {
   name: 'App',
   components: { Navbar, Login },
   data: () => ({
+        links: [
+      { icon: 'home', text: 'Home', route: '/'},
+      AccountRoutes.menuItem,
+      PurchaseRoutes.menuItem,
+    ]
 //    firebase: firebase,
     //
   }),
