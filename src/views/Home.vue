576f1496547ce4380f3325c0a17f9b062730ebf8
Takeyuki UEDA <ueda@latelierdueda.com>
2021-07-11 12:24:16 +0900

    revised

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,8 @@
     <ul>
       <li><b>Sign in</b> can be available by <b>email address</b> with verification.</li>
       <li>Created account can be <b>changed</b>, <b>deleted</b> at <b>Account</b> page.</li>
-      <li>Notice that the Account page is just a blanck <b>before login</b>.</li>
-      <li>All feature you are seeing here is available with <a href="https://www.npmjs.com/package/vuetify-nav">vuetify-nav</a> and  <a href="https://www.npmjs.com/package/vue-faui-user-fe2">vue-faui-user-fe2
+      <li>Notice that the Account page is just a blanck <b>before</b> login.</li>
+      <li>All feature you are seeing here is available with the npm package <a href="https://www.npmjs.com/package/vuetify-nav">vuetify-nav</a> and  <a href="https://www.npmjs.com/package/vue-faui-user-fe2">vue-faui-user-fe2
 </a>.</li>
       <li>You can refer <a href="https://github.com/UedaTakeyuki/vue-faui-user-fe-sample">source code</a> of this site for your implementation.</li>
       <li>Feel free to ask any question and report issue <a href="https://github.com/UedaTakeyuki/vue-faui-user-fe-sample/issues">here</a>.</li>
